f8319b5ceb1497dc2651c380df89637ba390a7a6
Surma <surma@surma.link>
2016-12-06 13:43:13 +0000

    feat(concat): Make concat work with any number of collections

--- f/concatLazy.js
+++ f/concatLazy.js
@@ -1,4 +1,3 @@
-function* concat(it1, it2) {
-  for (let v of it1) yield v;
-  for (let v of it2) yield v;
+function* concat(...its) {
+  for (let it of its) yield* it;
 }
--- test/concat.test.js
+++ test/concat.test.js
@@ -1,3 +1,3 @@
 it('should concatenate', function () {
-  expect(f([1,2,3],[4,5,6])).to.deep.equal([1,2,3,4,5,6]);
+  expect(f([1,2,3],[4,5,6],[7,8,9])).to.deep.equal([1,2,3,4,5,6,7,8,9]);
 });
