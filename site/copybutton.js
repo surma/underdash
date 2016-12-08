07d3458f92afd19184bb1c723104730e2fe2d1bd
Surma <surma@surma.link>
2016-12-08 14:19:51 +0000

    fix(site): Fix copy button

--- site/copybutton.js
+++ site/copybutton.js
@@ -1,18 +1,19 @@
 (function () {
   Array.from(document.querySelectorAll('button.copy')).forEach(btn => {
-    btn.onclick = evt => {
+    btn.onclick = event => {
       const btn = event.target;
       const ctr = event.target.parentNode;
+      const code = ctr.querySelector('pre');
       const ta = document.createElement('textarea');
       ctr.removeChild(btn);
-      ta.value = ctr.textContent;
+      ta.value = code.textContent;
       document.body.appendChild(ta);
       ta.select();
       try {
         document.execCommand('copy');
       } catch(e) {}
       document.body.removeChild(ta);
-      ctr.prepend(btn);
+      ctr.insertBefore(btn, code);
       requestAnimationFrame(_ => btn.focus());
     };
   });
